66c1a3c1049524ed5d83e03c595c3a7c01aea40e
Florian J. Breunig <Florian.J.Breunig@my-flow.com>
2026-01-25 13:49:56 +0100

    Call Mosh/Zellij on top of plain Bash

--- .config/fish/functions/zmosh.fish
+++ .config/fish/functions/zmosh.fish
@@ -1,3 +1,9 @@
 function zmosh
-    mosh $argv -- zellij attach --create
+    # Ensure we have at least one argument (the host)
+    if not set -q argv[1]
+        echo "Usage: zmosh <user@host>"
+        return 1
+    end
+
+    mosh $argv -- zellij attach --create --index 0
 end
