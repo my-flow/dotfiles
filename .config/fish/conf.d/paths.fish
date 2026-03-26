604f6c71c1b61bb6c8e470b409853d00da0b7a8e
Florian J. Breunig <florian@my-flow.com>
2026-03-26 23:17:43 +0100

    Resolve TexLive path dynamically by architecture and version

--- .config/fish/conf.d/paths.fish
+++ .config/fish/conf.d/paths.fish
@@ -5,5 +5,9 @@ switch (uname)
         fish_add_path /Applications/kdiff3.app/Contents/MacOS
         fish_add_path /Library/TeX/texbin
     case Linux
-        fish_add_path /usr/local/texlive/2025/bin/aarch64-linux
+        set -l arch (uname -m)
+        set -l texlive_dirs /usr/local/texlive/*/bin/$arch-linux 2>/dev/null
+        if set -q texlive_dirs[1]
+            fish_add_path $texlive_dirs[-1]
+        end
 end
